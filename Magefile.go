7092b2def5aaf8e63e76791cec3157954a86d862
Giuseppe Guerra <giuseppe@guerra.in>
2025-10-15 12:31:51 +0200

    remove github targets from magefile

--- Magefile.go
+++ Magefile.go
@@ -4,22 +4,16 @@ package main
 
 import (
 	"archive/zip"
-	"context"
 	"errors"
 	"fmt"
 	"io"
 	"io/fs"
-	"net/http"
 	"os"
 	"path/filepath"
 	"runtime"
-	"strconv"
 	"strings"
 	"sync"
-	"time"
 
-	"github.com/bradleyfalzon/ghinstallation/v2"
-	"github.com/google/go-github/v53/github"
 	"github.com/magefile/mage/mg"
 	"github.com/magefile/mage/sh"
 )
@@ -29,16 +23,13 @@ type Build mg.Namespace
 const (
 	distFolder      = "dist"
 	artifactsFolder = "artifacts"
-
-	gitHubOrg  = "grafana"
-	gitHubRepo = "detect-angular-dashboards"
+	programName     = "detect-angular-dashboards"
 )
 
 // Go builds the go binary for the specified os and arch into dist/<os>_<arch>/detect-angular-dashboards.
 func (Build) Go(goOs, goArch string) error {
 	fmt.Println("building for", goOs, goArch)
 
-	const programName = "detect-angular-dashboards"
 	args := []string{"build", "-o", filepath.Join(distFolder, goOs+"_"+goArch, programName)}
 
 	ldFlags := []string{"-s", "-w"}
@@ -127,7 +118,7 @@ func (b Build) zipFolder(inFolder string, outFileName string) error {
 
 // Docker builds the docker image with the specified tag.
 func (Build) Docker(tag string) error {
-	return sh.RunV("docker", "build", "-t", "detect-angular-dashboards:"+tag, ".")
+	return sh.RunV("docker", "build", "-t", programName+":"+tag, ".")
 }
 
 // Package runs build:all and creates multiple .zip files inside dist/artifacts/<releaseName>, one for each folder in dist/*.
@@ -161,7 +152,7 @@ func Package(releaseName string) error {
 		if err := os.MkdirAll(outFolder, os.ModePerm); err != nil {
 			return fmt.Errorf("mkdir %q: %w", outFolder, err)
 		}
-		zipFn := filepath.Join(outFolder, fmt.Sprintf("%s_%s_%s.zip", gitHubRepo, releaseName, d.Name()))
+		zipFn := filepath.Join(outFolder, fmt.Sprintf("%s_%s_%s.zip", programName, releaseName, d.Name()))
 
 		wg.Add(1)
 		go func() {
@@ -204,123 +195,6 @@ func Lint() error {
 	}
 
 	return nil
-}
-
-type GitHub mg.Namespace
-
-// Release pushes a GitHub release
-func (g GitHub) Release(releaseName string) error {
-	mg.Deps(mg.F(Package, releaseName))
-
-	// Determine files to upload
-	artifactsRoot := filepath.Join(distFolder, artifactsFolder, releaseName)
-	toUploadFileNames := map[string]struct{}{}
-	if err := filepath.WalkDir(artifactsRoot, func(path string, d fs.DirEntry, err error) error {
-		if path == artifactsRoot {
-			// Skip folder itself
-			return nil
-		}
-		if d.IsDir() {
-			// Do not recurse
-			return filepath.SkipDir
-		}
-		if filepath.Ext(d.Name()) != ".zip" {
-			// Skip non-zip files
-			return nil
-		}
-		toUploadFileNames[filepath.Join(artifactsRoot, d.Name())] = struct{}{}
-		return nil
-	}); err != nil {
-		return fmt.Errorf("walkdir: %w", err)
-	}
-
-	// Ensure we have files to attach to the release
-	if len(toUploadFileNames) == 0 {
-		return fmt.Errorf("could not find artifacts to upload for %q", releaseName)
-	}
-
-	// Check and get GitHub app env vars
-	var ghAppID, ghInstallationID int64
-	for _, o := range []struct {
-		dst    *int64
-		envVar string
-	}{
-		{&ghAppID, "GITHUB_APP_ID"},
-		{&ghInstallationID, "GITHUB_APP_INSTALLATION_ID"},
-	} {
-		var err error
-		v := os.Getenv(o.envVar)
-		*o.dst, err = strconv.ParseInt(v, 10, 64)
-		if err != nil {
-			return fmt.Errorf("%q (value of env var %q) is not an integer", v, o.envVar)
-		}
-	}
-
-	// Create GitHub client
-	ghTransport, err := ghinstallation.New(http.DefaultTransport, ghAppID, ghInstallationID, []byte(os.Getenv("GITHUB_APP_PRIVATE_KEY")))
-	if err != nil {
-		return fmt.Errorf("ghinstallation new: %w", err)
-	}
-	ghClient := github.NewClient(&http.Client{Transport: ghTransport})
-	ctx, canc := context.WithTimeout(context.Background(), time.Minute*10)
-	defer canc()
-
-	// Create release
-	release, _, err := ghClient.Repositories.CreateRelease(ctx, gitHubOrg, gitHubRepo, &github.RepositoryRelease{
-		Name:       github.String(releaseName),
-		TagName:    github.String(releaseName),
-		Draft:      github.Bool(false),
-		Prerelease: github.Bool(false),
-		MakeLatest: github.String("true"),
-	})
-	if err != nil {
-		return fmt.Errorf("create github release: %w", err)
-	}
-	fmt.Println("created github release", releaseName)
-
-	// Set up error handling
-	var finalErr error
-	errs := make(chan error)
-	go func() {
-		for err := range errs {
-			finalErr = errors.Join(finalErr, err)
-		}
-	}()
-
-	// Upload all artifacts and attach them to the release
-	var wg sync.WaitGroup
-	wg.Add(len(toUploadFileNames))
-	for fn := range toUploadFileNames {
-		fn := fn
-		go func() {
-			defer wg.Done()
-
-			fmt.Println("uploading", fn, "...")
-			f, err := os.Open(fn)
-			if err != nil {
-				errs <- fmt.Errorf("open %q: %w", fn, err)
-				return
-			}
-			defer func() {
-				if err := f.Close(); err != nil && !errors.Is(err, os.ErrClosed) {
-					errs <- fmt.Errorf("close %q: %w", fn, err)
-				}
-			}()
-
-			if _, _, err := ghClient.Repositories.UploadReleaseAsset(ctx, gitHubOrg, gitHubRepo, *release.ID, &github.UploadOptions{
-				Name: filepath.Base(fn),
-			}, f); err != nil {
-				errs <- fmt.Errorf("upload release artifact %q: %w", fn, err)
-				return
-			}
-			fmt.Println("upload", fn, "ok!")
-		}()
-	}
-
-	// Wait for upload goroutines to finish
-	wg.Wait()
-	close(errs)
-	return finalErr
 }
 
 // gitCommitSha returns the git commit sha for the current repo or "" if none.
